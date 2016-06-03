88e9feef726c050a32430cab3881cfbe9085abd5
James Keys <skolsuper@gmail.com>
2016-06-03 09:48:57 +0700

    Refactor ChangeListener helper class to better suit use as a library.

--- resync/listener.py
+++ resync/listener.py
@@ -1,8 +1,9 @@
 import asyncio
+from collections import Awaitable
 from logging import getLogger
 from typing import Callable
 
-from rethinkdb import ReqlDriverError, ReqlRuntimeError, ReqlQueryLogicError
+from rethinkdb import ReqlTimeoutError, ReqlAvailabilityError
 
 from resync.diff import Diff
 from resync.models import Model
@@ -17,31 +18,28 @@ class ChangeListener:
     Recovers from connection being dropped.
     """
 
-    def __init__(self, queryset: Queryset, callback: Callable[[Model, Diff], None], timeout: asyncio.Future):
+    def __init__(self, queryset: Queryset, callback: Callable[[Model, Diff], Awaitable]):
         """
         Args:
             queryset: The queryset on which to listen for changes.
-            callback: The callback to call with each change.  Should have the signature (Model, Diff) -> None
-            timeout: A future that when cancelled will stop the listener at next error (which will be raised when the
-                     database connection is closed.)
+            callback: The callback to call with each change.  Should have the signature (Model, Diff) -> Awaitable
         """
         self.queryset = queryset
         self.callback = callback
-        self.timeout = timeout
 
     async def listen(self):
-        while not self.timeout.cancelled():
-            try:
-                async for obj, diff in self.queryset.changes():
-                    await self.callback(obj, diff)
-            except ReqlQueryLogicError:
-                l.exception('This query is fundamentally stupid.')
-                raise KeyboardInterrupt  # This kills the recon. (but allows finalizers to run)
-            except (ReqlDriverError, ReqlRuntimeError):
-                l.debug('ReqlError in listener coroutine for {}'.format(self.queryset.model), exc_info=True)
-                await asyncio.sleep(1)  # Prevent runaway CPU and memory usage if database fails terminally.
-                # TODO: Implement a limit on the number of retries
-        l.debug('Shutting down {}'.format(self))
+        try:
+            while True:
+                try:
+                    async for obj, diff in self.queryset.changes():
+                        await self.callback(obj, diff)
+                except (ReqlTimeoutError, ReqlAvailabilityError):
+                    l.debug('ReqlError in listener {}'.format(self), exc_info=True)
+                    await asyncio.sleep(1)  # Prevent busy looping if database fails terminally.
+                    # TODO: Implement some kind of backoff strategy
+                    # TODO: Implement a limit on the number of retries
+        finally:
+            l.debug('Shutting down {}'.format(self))
 
     def __str__(self):
         return 'ChangeListener for {}'.format(self.queryset)
